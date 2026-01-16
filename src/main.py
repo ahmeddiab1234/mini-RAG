b48161ae081d6c5cc8036fe699eddb587fcc5ed0
ahmeddiab1234 <shhth0034@gmail.com>
2026-01-16 15:56:02 +0200

    modify router

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 app = FastAPI()
 
-@app.on_event("startup")
 async def startup_db_client():
 
     settings = get_settings()
@@ -25,10 +24,11 @@ async def startup_db_client():
     app.embedding_client = llm_provider_factory.create( provider=settings.EMBEDDING_BACKEND )
     app.embedding_client.set_embedding_model(model_id = settings.EMBEDDING_MODEL_ID, embedding_size = settings.EMBEDDING_MODEL_SIZE)
 
-@app.on_event("shutdown")
 async def shutdown_db_client():
     app.mongo_conn.close()
 
+app.router.lifespan.on_startup.append(startup_db_client)
+app.router.lifespan.on_shutdown.append(shutdown_db_client)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
